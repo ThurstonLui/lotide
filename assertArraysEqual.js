ef03d0fc0834b767d1d3cd6c1541fb6864e6158d
Thurston Lui <thurstonlui@gmail.com>
2020-09-16 03:28:26 +0000

    created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1, array2) {
+  //Create version of arrays that can be compared (i.e. string)
+  
+  //Assuming lengths are the same, check if each element of the arrays is the same
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }        
+    }
+    return true;
+  }
+} 
+
+//Compare two arrays
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(eqArrays(array1,array2) + " === true")
+  }
+  else {
+    console.log(eqArrays(array1, array2) + " === false")
+  }
+} 
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [3, 2, 1])
