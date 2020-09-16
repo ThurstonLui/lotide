76ad6953bd3fda45f26161f1e3d2dd0bf8555dc1
Thurston Lui <thurstonlui@gmail.com>
2020-09-16 02:38:35 +0000

    compared arrays

---
+++ eqArrays.js
@@ -0,0 +1,50 @@
+//Function that takes in two arrays and returns true if they are the same
+
+const arrayConverter = function(arrayToString) {
+  return arrayToString.join();
+}
+
+const eqArrays = function(array1, array2) {
+    //Create version of arrays that can be compared (i.e. string)
+    /*console.log(arrayConverter(array1));
+    console.log(arrayConverter(array2));
+    
+    if (arrayConverter(array1) === arrayConverter(array2)) {
+      return true;
+    }
+
+  return false;*/
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }  
+    }
+  } 
+
+  return true;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`😁️Assertion Passed: ${actual} === ${expected}`);
+  }
+  
+  else if (actual !== expected) {
+    console.log(`😡️ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
