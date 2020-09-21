40c7bf9ed1cc61dd19feea6f1bf70520bc788e5a
Thurston Lui <thurstonlui@gmail.com>
2020-09-21 21:55:35 +0000

    updated tail and middle tests with some chai tea

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([2,3,7,9,4,5]), [7,9]);
-assertArraysEqual(middle([2,3,7,4,4,5]), [7,9]);
+describe("#tail", () => {
+  it("returns [7, 9] for 2,3,7,9,4,5]", () => {
+    assert.deepEqual(middle([2,3,7,9,4,5]), [7,9]);
+  });
+  it("returns [3] for [2,3,5]", () => {
+    assert.deepEqual(middle([2,3,5]), [3]); 
+  });
+});
+
+// assertArraysEqual(middle([2,3,7,9,4,5]), [7,9]);
+// assertArraysEqual(middle([2,3,7,4,4,5]), [7,9]);
 
 
-assertArraysEqual(middle([2,3,5]), [3]);
-assertArraysEqual(middle([2,3,7,9,4,5]), [7,8]);
+// assertArraysEqual(middle([2,3,5]), [3]);
+// assertArraysEqual(middle([2,3,7,9,4,5]), [7,8]);
